d0a930b035346e6f0504fbe32a8652e0c198cb19
Milan <comiv92@gmail.com>
2021-06-10 16:40:37 +0200

    Feat: Add types and constants

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
+const COMMENTS_URL = 'https://jsonplaceholder.typicode.com/comments';
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users';
+
+
+export {
+  POSTS_URL,
+  COMMENTS_URL,
+  USERS_URL,
+}
